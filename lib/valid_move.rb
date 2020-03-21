470a2e6732b212e0329f9f4420960ddf60ca83d3
Sam Tamburri <tamburrisam@gmail.com>
2020-03-21 19:02:54 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-
 
 def valid_move?(board, index)
-  index.between?(0, 8) && !(position_taken?(board, index))
+  index.between?(0, 8) !(position_taken?(board, index))
 end
 
 def position_taken?(board, index)
  !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
-
-
-=======
-def valid_move?(board, position)
->>>>>>> 62694efe7aa34b4b3305dba0d6a11cff85efbd9e
